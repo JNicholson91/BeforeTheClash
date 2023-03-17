2eba7ac7f0a7d49e2298b9cf80b65a2f464942ae
John Nicholson <jnicholsonva@gmail.com>
2023-03-17 17:57:39 -0400

    Added pages for individual posts and some basic design

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.utils import timezone
 
 # Create your views here.
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_details.html', {'post': post} )
